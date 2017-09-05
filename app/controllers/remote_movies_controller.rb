8d4e3b811ec25f18b829b33cd37a8429c260dff9
Francesco Mari <francescojjmari@gmail.com>
2017-09-05 15:59:11 +0200

    Add RemoteMoviesController, search feature

--- app/controllers/remote_movies_controller.rb
+++ app/controllers/remote_movies_controller.rb
@@ -1,4 +1,15 @@
 class RemoteMoviesController < ApplicationController
   def index
+    keyword = params[:search]
+    response = HTTParty.get("https://api.themoviedb.org/3/search/movie?api_key=#{Rails.application.secrets.tmdb_api_key}&query=#{keyword}&adult=false")
+
+    if response.code == 200
+      response_hash = response.to_hash["results"]
+      @movies = []
+      response_hash.each do |movie|
+        @movies << { title: movie["title"], tmdb_id: movie["id"] }
+      end
+    end
+
   end
 end
