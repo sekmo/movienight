cec20ef1d531c78e2175b1bd2f16a6ac31535a1f
Francesco Mari <francescojjmari@gmail.com>
2017-10-05 17:28:58 +0200

    Refactor WishesController#create

--- app/controllers/wishes_controller.rb
+++ app/controllers/wishes_controller.rb
@@ -7,8 +7,8 @@ class WishesController < ApplicationController
   end
 
   def create
-    movie = Movie.where(tmdb_code: params[:tmdb_code]).first_or_create!(title:       params[:title],
-                                                                        poster_path: params[:poster_path])
+    movie = Movie.create_with(title: params[:title], poster_path: params[:poster_path])
+                 .find_or_create_by!(tmdb_code: params[:tmdb_code])
     wish = Wish.find_or_initialize_by(movie: movie, user: current_user)
     if wish.persisted?
       flash[:notice] = "The movie was already present in your wishlist."
