53499e7b8e8a26d1ae599ed71751442fc09313e1
Francesco Mari <francescojjmari@gmail.com>
2017-11-23 12:45:36 +0100

    Add check_owner method in appcontroller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
   before_action :authenticate_user!
+
+  protected
+
+  def redirect_to_root_with_error
+    flash[:error] = "Error in your request"
+    redirect_to root_url
+  end
+
+  def check_owner(object)
+    if object.user != current_user
+      redirect_to_root_with_error
+    end
+  end
 end
