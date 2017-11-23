d8b1ecbbcf8df5314b833cea9248c0a8041af66c
Francesco Mari <francescojjmari@gmail.com>
2017-11-23 12:33:21 +0100

    Resourceful routes for wishes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,7 @@
 Rails.application.routes.draw do
-  get  'wishes', to: 'wishes#index'
-  get  'wishes/new'
-  post 'wishes', to: 'wishes#create'
-
   get 'remote_movies', to: 'remote_movies#index'
-
+  resources :wishes
   devise_for :users
-
   get  'pages/:page', to: 'pages#show', as: 'pages'
   root 'pages#show', page: 'welcome'
 end
