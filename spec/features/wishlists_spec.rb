2aa9397604f2f3569a9d60657f91f9f6b17ef073
Francesco Mari <francescojjmari@gmail.com>
2017-11-23 12:52:49 +0100

    Add 'remove movies from wishlist' spec

---
+++ spec/features/wishlists_spec.rb
@@ -0,0 +1,30 @@
+require "rails_helper"
+
+RSpec.feature "Wishlist", type: :feature do
+  before(:each) do
+    @user ||= create(:user)
+    login_as @user, scope: :user
+  end
+
+  scenario "User adds a movie to her wishlist" do
+    visit new_wish_path
+    fill_in "search", with: "kill bill"
+    click_on "Search"
+    expect(page).to have_content("Kill Bill: Vol. 1")
+    expect { click_on "Kill Bill: Vol. 1" }.to change { @user.wishes.count }.by(1)
+  end
+
+  scenario "User removes a movie from her wishlist" do
+    create(:wish, user: @user)
+    movie = create(:movie)
+    create(:wish, movie: movie, user: @user)
+    visit wishes_path
+    movie_span = find("span", text: movie.title)
+    movie_span_container = movie_span.first(:xpath, ".//..")
+    expect {
+      movie_span_container.click_on("Remove movie")
+    }.to change { @user.wishes.count }.by(-1)
+    expect(page).to have_content "The movie was successfully removed from your wishlist."
+    expect(page).not_to have_content "Kill Bill: Vol. 1"
+  end
+end
