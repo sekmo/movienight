8f898db9a2e56bcd9831b563514578fc1cb4ec2e
Francesco Mari <francescojjmari@gmail.com>
2018-05-01 12:00:09 +0200

    User can update a profile

--- spec/features/profile_spec.rb
+++ spec/features/profile_spec.rb
@@ -23,4 +23,21 @@ RSpec.feature "Wishlist", type: :feature do
     expect(current_path).to eql(new_wish_path)
     expect(page).to have_content "Profile was successfully created. Now create your wishlist!"
   end
+
+  scenario "User can update her profile" do
+    @profile = create(:profile, user: @user, first_name: "Francesco", last_name: "Mari",
+      nickname: "sekmo")
+    visit edit_profile_path
+    fill_in "First name", with: "Mario"
+    fill_in "Last name", with: "Rossi"
+    fill_in "Nickname", with: "marione"
+
+
+    expect{
+      click_on "Update Profile"
+      @profile.reload
+    }.to change  { @profile.first_name }.from("Francesco").to("Mario")
+     .and change { @profile.last_name }.from("Mari").to("Rossi")
+     .and change { @profile.nickname }.from("sekmo").to("marione")
+  end
 end
