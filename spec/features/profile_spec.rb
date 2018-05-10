2a0ff0cb3495faaf8ab8fb454b3ff7581eaf2954
Francesco Mari <francescojjmari@gmail.com>
2018-05-10 11:12:22 +0200

    :white_check_mark: Reorder profile specs

--- spec/features/profile_spec.rb
+++ spec/features/profile_spec.rb
@@ -6,12 +6,6 @@ RSpec.feature "Wishlist", type: :feature do
     login_as @user, scope: :user
   end
 
-  scenario "User cannot search for a movie if she doesn't have a profile" do
-    visit new_wish_path
-    expect(current_path).to eql(new_profile_path)
-    expect(page).to have_content "Create a profile to add movies to your wishlist!"
-  end
-
   scenario "User can create a profile" do
     visit new_profile_path
     fill_in "First name", with: "Francesco"
@@ -24,22 +18,14 @@ RSpec.feature "Wishlist", type: :feature do
     expect(page).to have_content "Profile was successfully created. Now create your wishlist!"
   end
 
-  scenario "User cannot visit the new profile page if she already have a profile" do
-    @profile = create(:profile, user: @user, first_name: "Francesco", last_name: "Mari",
-      nickname: "sekmo")
-    visit new_profile_path
-    expect(page).to have_content "You already have a profile. Create your wishlist!"
-    expect(current_path).to eql(new_wish_path)
-  end
-
   scenario "User can update her profile" do
     @profile = create(:profile, user: @user, first_name: "Francesco", last_name: "Mari",
       nickname: "sekmo")
     visit edit_profile_path
+
     fill_in "First name", with: "Mario"
     fill_in "Last name", with: "Rossi"
     fill_in "Nickname", with: "marione"
-
 
     expect{
       click_on "Update Profile"
@@ -47,5 +33,19 @@ RSpec.feature "Wishlist", type: :feature do
     }.to change  { @profile.first_name }.from("Francesco").to("Mario")
      .and change { @profile.last_name }.from("Mari").to("Rossi")
      .and change { @profile.nickname }.from("sekmo").to("marione")
+  end
+
+  scenario "User cannot search for a movie if she doesn't have a profile" do
+    visit new_wish_path
+    expect(current_path).to eql(new_profile_path)
+    expect(page).to have_content "Create a profile to add movies to your wishlist!"
+  end
+
+  scenario "User cannot visit the new profile page if she already have a profile" do
+    @profile = create(:profile, user: @user, first_name: "Francesco", last_name: "Mari",
+      nickname: "sekmo")
+    visit new_profile_path
+    expect(page).to have_content "You already have a profile. Create your wishlist!"
+    expect(current_path).to eql(new_wish_path)
   end
 end
