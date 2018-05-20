95af85953a02059adff76a94b2ea0ba51aa05997
Francesco Mari <francescojjmari@gmail.com>
2018-05-20 17:07:18 +0200

    Add seeds - friendship requests

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+if !Rails.env.production?
+  ActiveRecord::Base.connection.tables.each do |table|
+    if !table.in? ["schema_migrations", "ar_internal_metadata"]
+      puts "Truncating #{table}."
+      ActiveRecord::Base.connection.execute("TRUNCATE TABLE #{table} RESTART IDENTITY CASCADE;")
+    end
+  end
+end
+
+# Friendship.destroy_all
+# Profile.destroy_all
+# User.destroy_all
+
+User.create!([
+  { email: "francesco@example.com", password: "password", password_confirmation: "password" },
+  { email: "chiara@example.com", password: "password", password_confirmation: "password" },
+  { email: "mario@example.com", password: "password", password_confirmation: "password" },
+  { email: "andrea@example.com", password: "password", password_confirmation: "password" }
+])
+
+Profile.create!([
+  { first_name: "Francesco", last_name: "Mari", nickname: "sekmo", user: User.find(1) },
+  { first_name: "Chiara", last_name: "Sag", nickname: "chiarasag", user: User.find(2) },
+  { first_name: "Mario", last_name: "Rossi", nickname: "marione", user: User.find(3) },
+  { first_name: "Andrea", last_name: "Verdi", nickname: "andreav", user: User.find(4) }
+])
+
+User.find(2).ask_friendship(User.find(1))
+User.find(3).ask_friendship(User.find(1))
+User.find(4).ask_friendship(User.find(1))
+Friendship.last.confirm!
