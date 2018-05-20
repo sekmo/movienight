fcb42d6dec3bdecbf5dc3e4412554f3adb10cbe8
Francesco Mari <francescojjmari@gmail.com>
2018-05-20 17:05:34 +0200

    Add FriendshipsController

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'remote_movies', to: 'remote_movies#index'
-  resources :wishes
+  resources :wishes, only: [:new, :index, :create, :update, :destroy]
+  resources :friendships
   devise_for :users
   resource :profile, only: [:show, :create, :new, :edit, :update]
   resolve('Profile') { [:profile] }
