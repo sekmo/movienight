47b5cbb0e39aa6e2ab91a61865868d2ed22c9e68
Francesco Mari <francescojjmari@gmail.com>
2018-05-20 15:21:47 +0200

    Fix Profile factory

--- spec/factories/profile.rb
+++ spec/factories/profile.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :profile do
-    first_name Faker::Name.unique.first_name
-    last_name Faker::Name.unique.last_name
+    first_name { Faker::Name.unique.first_name }
+    last_name { Faker::Name.unique.last_name }
     nickname { first_name.downcase }
     user
   end
