3ccc1eecae5b48f78bbee4a64a971e56f6b00498
Francesco Mari <francescojjmari@gmail.com>
2018-05-20 17:03:54 +0200

    Add friendship-related methods to User

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,4 +9,30 @@ RSpec.describe User, type: :model do
       expect(build(:user, profile: nil)).to be_valid
     end
   end
+
+  describe "#ask_friendship" do
+    it "creates a new friendship" do
+      @tom   ||= create(:user, :with_profile)
+      @jerry ||= create(:user, :with_profile)
+
+      expect {
+        @tom.ask_friendship(@jerry)
+      }.to change { @jerry.reload.friendship_requesters.include?(@tom) }.from(false).to(true)
+    end
+  end
+
+  describe "#friends" do
+    #TODO
+    it "returns all the friends of the user"
+  end
+
+  describe "#friendship_requests" do
+    #TODO
+    it "returns the pending friendship requests"
+  end
+
+  describe "#friendship_requesters" do
+    #TODO
+    it "returns the users that made a friendship request to the user"
+  end
 end
