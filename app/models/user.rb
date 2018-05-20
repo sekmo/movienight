3e001ace48ec5d50c4742633778ba221579f115e
Francesco Mari <francescojjmari@gmail.com>
2018-05-20 17:03:54 +0200

    Add friendship-related methods to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,25 +6,26 @@ class User < ApplicationRecord
 
   has_many :wishes, dependent: :destroy
 
-  has_many :outgoing_friendships, class_name: "FriendshipRequest", foreign_key: "sender_id"
-  has_many :outgoing_friends, through: :outgoing_friendships
-
-  has_many :incoming_friendships, class_name: "FriendshipRequest", foreign_key: "recipient_id"
-  has_many :incoming_friends, through: :incoming_friendships
+  has_many :outgoing_friendships, class_name: "Friendship", foreign_key: "sender_id"
+  has_many :incoming_friendships, class_name: "Friendship", foreign_key: "recipient_id"
 
   has_one :profile
 
   def ask_friendship(user)
-    FriendshipRequest.create(sender: self, recipient: user)
+    Friendship.create(sender: self, recipient: user)
   end
 
-  def accept_friendship(user)
-    friendship = incoming_friendships.find_by(sender: user)
-    friendship.accept!
+  def friends
+    friends = outgoing_friendships.select(&:confirmed?).map(&:recipient) +
+                incoming_friendships.select(&:confirmed?).map(&:sender)
   end
 
-  def decline_friendship(user)
-    friendship = incoming_friendships.find_by(sender: user)
-    friendship.destroy!
+  def friendship_requests
+    incoming_friendships.select(&:pending?)
+  end
+
+  def friendship_requesters
+    #TODO add a join here - avoid select?
+    friendship_requests.map(&:sender)
   end
 end
