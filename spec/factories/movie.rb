e902b224d6bf867730627d4ee624eadb8bfa0f1e
Francesco Mari <francescojjmari@gmail.com>
2018-05-26 10:44:32 +0200

    Move to system tests

--- spec/factories/movie.rb
+++ spec/factories/movie.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :movie do
-    sequence(:title){ |n| "Kill Bill vol: #{n} - #{Faker::Book.title}" }
+    sequence(:title){ |n| "Kill Bill vol: #{n}" }
     sequence(:tmdb_code) { |n| n }
   end
 end
