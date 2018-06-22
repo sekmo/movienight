cf686386799dd21fed1852b7e98a72e3e323857d
Francesco Mari <francescojjmari@gmail.com>
2018-06-22 17:57:05 +0200

    Improve ProfileMatcher performance

--- app/models/profile_matcher.rb
+++ app/models/profile_matcher.rb
@@ -1,23 +1,20 @@
 class ProfileMatcher
-  def initialize(user)
-    Rails.logger.debug("ProfileMatcher#initialize")
-    Rails.logger.debug("user.friends.map(&:profile)")
-    @friends_profiles = Profile.where(user_id: user.friends_ids)
+  attr_reader :matched_user
 
-    Rails.logger.debug("user.friendship_requesters.map(&:profile)")
-    @friendship_requesters = user.friendship_requesters.map(&:profile)
-
-    Rails.logger.debug("user.friendship_receivers.map(&:profile)")
-    @friendship_receivers = user.friendship_receivers.map(&:profile)
+  def initialize(matched_user)
+    @matched_user = matched_user
+    @friends_profiles = Profile.where(user_id: matched_user.friends_ids)
+    @friendship_requesters = Profile.where(user_id: matched_user.friendship_requesters_ids)
+    @friendship_receivers = Profile.where(user_id: matched_user.friendship_receivers_ids)
   end
 
   def match(profile)
     return { match_kind: :friend } if @friends_profiles.include?(profile)
     if @friendship_requesters.include?(profile)
-      friendship = Friendship.find_by(sender: profile.user)
+      friendship = Friendship.find_by(sender: profile.user, recipient: @matched_user)
       return { match_kind: :requester, friendship: friendship }
     end
     return { match_kind: :receiver } if @friendship_receivers.include?(profile)
-    return { match_kind: :no_match }
+    return { match_kind: :stranger }
   end
 end
