7fa0fb339f7bf6a6e5be79f0bfe476d279c484fd
Francesco Mari <francescojjmari@gmail.com>
2018-07-15 22:00:29 +0200

    Add PublicPofiles controller

---
+++ app/controllers/public_profiles_controller.rb
@@ -0,0 +1,21 @@
+class PublicProfilesController < ApplicationController
+  def index
+    term = params[:q]
+    redirect_to_root_with_error if term.blank?
+    @profiles = Profile.search_by_full_name(term) - [current_user.profile]
+  end
+
+  def show
+    set_profile
+    @friends = @profile.user.friends
+  end
+
+  private
+
+  def set_profile
+    @profile = Profile.find(params[:id])
+    check_owner(@profile)
+  rescue ActiveRecord::RecordNotFound
+    redirect_to_root_with_error
+  end
+end
