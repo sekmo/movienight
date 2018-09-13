a197efdd1c6fda1c03428c323aeab6dc1b0ebb51
Francesco Mari <francescojjmari@gmail.com>
2018-09-13 23:10:15 +0200

    :white_check_mark: Fix wishlist system specs

--- spec/system/wishlists_spec.rb
+++ spec/system/wishlists_spec.rb
@@ -1,14 +1,15 @@
 require "rails_helper"
 
 RSpec.describe "Managing the wishlist", type: :system do
+  let(:user) { create(:user, :with_profile) }
+  let(:profile) { user.profile }
+
   before(:each) do
-    user = create(:user, :with_profile)
-    @profile = user.profile
     login_as user, scope: :user
   end
 
   scenario "User cannot search for a movie if she doesn't have a profile" do
-    @profile.destroy
+    profile.destroy
     visit new_wish_path
     expect(current_path).to eql(new_profile_path)
     expect(page).to have_content "Create your social profile to continue"
@@ -21,26 +22,27 @@ RSpec.describe "Managing the wishlist", type: :system do
     click_on "Search"
 
     expect(page).to have_content("Kill Bill: Vol. 1")
-    movie_span = find("span", text: "Kill Bill: Vol. 1")
+
+    movie_span = page.all("span", text: "Kill Bill: Vol. 1").first
     movie_span_container = movie_span.first(:xpath, ".//..")
     expect {
       movie_span_container.click_on("Add to wishlist")
-    }.to change { @profile.wishes.count }.by(1)
+    }.to change { profile.wishes.count }.by(1)
   end
 
   scenario "User removes a movie from her wishlist", js: true do
     movie = create(:movie)
-    create(:wish, movie: movie, profile: @profile)
+    create(:wish, movie: movie, profile: profile)
     visit wishes_path
     expect(page).to have_content movie.title
 
     movie_li = find("li", text: movie.title)
 
-    expect(@profile.wishes.count).to eq(1)
+    expect(profile.wishes.count).to eq(1)
 
     movie_li.click_on("Remove movie")
     expect(page).not_to have_content movie.title
-    expect(@profile.wishes.count).to eq(0)
+    expect(profile.wishes.count).to eq(0)
 
     # TODO Why doesn't it work?
     # expect {
