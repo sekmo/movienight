322cea3aee93c5e690d524ae5a34ea50bf62ffd2
Francesco Mari <francescojjmari@gmail.com>
2018-09-29 02:26:58 +0200

    Fix profile image visualization

--- app/helpers/profiles_helper.rb
+++ app/helpers/profiles_helper.rb
@@ -1,9 +1,9 @@
 module ProfilesHelper
   def profile_image_for(profile, class_name = "profile__picture")
     if profile.image.present?
-       image_tag profile.image_url, class: class_name
-     else
-       tag.div profile.initials, class: "#{class_name}--placeholder"
+      "<div class=\"#{class_name}\" style=\"background: url(#{profile.image_url})\"></div>".html_safe
+    else
+      tag.div profile.initials, class: "#{class_name}--placeholder"
     end
   end
 end
