b90bfc0cacd3da1b24c51c89dab8afc16ca1bad4
Francesco Mari <francescojjmari@gmail.com>
2018-10-24 22:38:25 +0200

    Refactor WishesController#create

--- app/controllers/wishes_controller.rb
+++ app/controllers/wishes_controller.rb
@@ -10,20 +10,9 @@ class WishesController < ApplicationController
   end
 
   def create
-    movie_details = TMDB::Client.get_movie_details(params[:tmdb_code])
-
-    movie = Movie.create_with(
-      title: movie_details["title"],
-      poster_path: movie_details["poster_path"],
-      length: movie_details["runtime"],
-      rating: movie_details["vote_average"],
-      year: movie_details["release_date"][0..3].to_i,
-      directors: movie_details["credits"]["crew"]
-        .select { |person| person["job"] == "Director" }
-        .map { |person| person["name"] }
-    ).find_or_create_by!(tmdb_code: params[:tmdb_code])
-
+    movie = Movie.find_or_create_by_tmdb_id(params[:tmdb_code])
     wish = Wish.find_or_initialize_by(movie: movie, profile: current_user_profile)
+    
     if wish.persisted?
       flash[:notice] = "The movie was already present in your wishlist."
     else
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -10,4 +10,20 @@ class Movie < ApplicationRecord
   def self.match_all_profiles(profile_ids)
     Movie.joins(:profiles).where(profiles: {id: profile_ids}).group(:id).having("COUNT(*) = ?", profile_ids.size)
   end
+
+  def self.find_or_create_by_tmdb_id(tmdb_id)
+    #TODO Don't do an api request if the movie is persisted
+    movie_details = TMDB::Client.get_movie_details(tmdb_id)
+
+    Movie.create_with(
+      title: movie_details["title"],
+      poster_path: movie_details["poster_path"],
+      length: movie_details["runtime"],
+      rating: movie_details["vote_average"],
+      year: movie_details["release_date"][0..3].to_i,
+      directors: movie_details["credits"]["crew"]
+        .select { |person| person["job"] == "Director" }
+        .map { |person| person["name"] }
+    ).find_or_create_by!(tmdb_code: tmdb_id)
+  end
 end
