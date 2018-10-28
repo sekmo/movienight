90038b343fa9bc253e80ee691ddcd29e1b923d7d
Francesco Mari <francescojjmari@gmail.com>
2018-10-28 16:06:32 +0100

    :ambulance: Fix for tmdb module

--- lib/TMDB/client.rb
+++ lib/TMDB/client.rb
@@ -1,42 +1,44 @@
-module TMDB::Client
-  def self.api_key
-    Rails.application.secrets.tmdb_api_key
-  end
-
-  def self.api_base_path
-    "https://api.themoviedb.org/3"
-  end
-
-  def self.search_movies(keyword)
-    response_body = make_request("/search/movie", "query=#{keyword}&adult=false")
-    response_hash = JSON.parse(response_body)["results"]
-
-    movies = []
-    response_hash.each do |movie|
-      movies << {
-        title: movie["title"],
-        tmdb_code: movie["id"],
-        poster_path: movie["poster_path"],
-        length: movie
-      }
-    end
-    movies
-  end
-
-  def self.get_movie_details(tmdb_code)
-    response_body = make_request("/movie/#{tmdb_code}", "append_to_response=credits,videos")
-    JSON.parse(response_body)
-  end
-
-  def self.make_request(endpoint, query_string)
-    url = "#{api_base_path}#{endpoint}?#{query_string}&api_key=#{api_key}"
-    response = HTTParty.get(url)
-
-    api_response_errors = JSON.parse(response.body)["errors"]
-    if response.code != 200 or api_response_errors
-      raise TMDB::RequestError.new(api_response_errors)
+module TMDB
+  module  Client
+    def self.api_key
+      Rails.application.secrets.tmdb_api_key
     end
 
-    response.body
+    def self.api_base_path
+      "https://api.themoviedb.org/3"
+    end
+
+    def self.search_movies(keyword)
+      response_body = make_request("/search/movie", "query=#{keyword}&adult=false")
+      response_hash = JSON.parse(response_body)["results"]
+
+      movies = []
+      response_hash.each do |movie|
+        movies << {
+          title: movie["title"],
+          tmdb_code: movie["id"],
+          poster_path: movie["poster_path"],
+          length: movie
+        }
+      end
+      movies
+    end
+
+    def self.get_movie_details(tmdb_code)
+      response_body = make_request("/movie/#{tmdb_code}", "append_to_response=credits,videos")
+      JSON.parse(response_body)
+    end
+
+    def self.make_request(endpoint, query_string)
+      url = "#{api_base_path}#{endpoint}?#{query_string}&api_key=#{api_key}"
+      response = HTTParty.get(url)
+
+      api_response_errors = JSON.parse(response.body)["errors"]
+      if response.code != 200 or api_response_errors
+        raise TMDB::RequestError.new(api_response_errors)
+      end
+
+      response.body
+    end
   end
 end
