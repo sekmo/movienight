2b671aae955706eb81dd78aac2b320d9d1044622
Francesco Mari <francescojjmari@gmail.com>
2019-05-17 09:04:09 +0200

    :nailcare: Nicer login page WIP

--- config/initializers/byebug.rb
+++ config/initializers/byebug.rb
@@ -1,8 +1,8 @@
 # config/initializer/byebug.rb
 if Rails.env.development?
-  require 'byebug/core'
-  Byebug.wait_connection = true
-  Byebug.start_server 'localhost', 3222
+  # require 'byebug/core'
+  # Byebug.wait_connection = true
+  # Byebug.start_server 'localhost', 3222
   # Connect with:
   # bundle exec byebug -R 3222
   # after the server has started
