5ff130121d5c7ad454b57ef2aee1e7edcc88ec45
Francesco Mari <francescojjmari@gmail.com>
2019-06-11 00:31:18 +0200

    Fix seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,15 +20,15 @@ end
 
 User.create!(
   [
-    { email: "francesco@example.com", password: "password", first_name: "Francesco", last_name: "Mari", nickname: "sekmo" },
-    { email: "chiara@example.com", password: "password", first_name: "Chiara", last_name: "Sag", nickname: "chiara" },
-    { email: "andrew@example.com", password: "password", first_name: "Andrew", last_name: "Black", nickname: "andrew" },
-    { email: "mark@example.com", password: "password", first_name: "Mark", last_name: "Stilton", nickname: "mark" },
-    { email: "michael@example.com", password: "password", first_name: "Micheal", last_name: "Green", nickname: "michael" },
-    { email: "arnold@example.com", password: "password", first_name: "Arnold", last_name: "White", nickname: "arnold" },
-    { email: "mattew@example.com", password: "password", first_name: "Mattew", last_name: "Lord", nickname: "mattew" },
-    { email: "patrick@example.com", password: "password", first_name: "Patrick", last_name: "King", nickname: "patrick" },
-    { email: "jack@example.com", password: "password", first_name: "Jack", last_name: "Steward", nickname: "jack" }
+    { email: "francesco@example.com", password: "password", first_name: "Francesco", last_name: "Mari", username: "sekmo" },
+    { email: "chiara@example.com", password: "password", first_name: "Chiara", last_name: "Sag", username: "chiara" },
+    { email: "andrew@example.com", password: "password", first_name: "Andrew", last_name: "Black", username: "andrew" },
+    { email: "mark@example.com", password: "password", first_name: "Mark", last_name: "Stilton", username: "mark" },
+    { email: "michael@example.com", password: "password", first_name: "Micheal", last_name: "Green", username: "michael" },
+    { email: "arnold@example.com", password: "password", first_name: "Arnold", last_name: "White", username: "arnold" },
+    { email: "mattew@example.com", password: "password", first_name: "Mattew", last_name: "Lord", username: "mattew" },
+    { email: "patrick@example.com", password: "password", first_name: "Patrick", last_name: "King", username: "patrick" },
+    { email: "jack@example.com", password: "password", first_name: "Jack", last_name: "Steward", username: "jack" }
   ]
 )
 
