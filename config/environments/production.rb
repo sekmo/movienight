faba6073e191f804c600a8597f08fbfd67d40472
sekmo <francescojjmari@gmail.com>
2020-05-21 17:32:51 +0200

    Fix ActionMailer default_url config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,7 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { host: 'http://getmovienight.com', port: 80 }
+  config.action_mailer.default_url_options = { host: 'https://getmovienight.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              "smtp.gmail.com",
