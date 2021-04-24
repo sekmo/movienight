9fe1ab60489349b15f743a46062ddffdea3f60bb
sekmo <francescojjmari@gmail.com>
2021-04-25 01:30:37 +0200

    Fix User factory

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :user do
     sequence(:email) { |n| "user#{n}@example.org" }
-    password { Faker::Internet.password(8) }
+    password { Faker::Internet.password }
     first_name { Faker::Name.unique.first_name }
     last_name { Faker::Name.unique.last_name }
     username { Faker::Name.unique.first_name.downcase }
